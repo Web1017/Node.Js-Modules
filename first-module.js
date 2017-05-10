550b83d06c8c1e581e8fa26a9ab115a2aab1204d
Javid <j-n_01@hotmail.com>
2017-05-10 12:32:47 -0400

    added module.export and referenced it. + exploring singleton pattern. Also installed node-inspector.

--- first-module.js
+++ first-module.js
@@ -1,5 +1,25 @@
+let tempData = "";
+
+//put data through first object and retrieve it from the 2nd object
 module.exports = {
+    put: function (d){
+        tempData = d;
+    },
+    get: function (){
+        return tempData;
+    },
+
     doIt: function(){
         console.log("did it");
+    },
+
+    doSomething: function(){
+        console.log("did something");
+    },
+
+ getItDone: function(){
+        console.log("Got it done");
     }
+
+
 };
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 'use strict';
-// const firstMod = require('./first-module');
 
-// firstMod.doIt();
+// For more Information on how modules was set up, Refer to following link.
+// ---> https://mva.microsoft.com/en-US/training-courses/mastering-nodejs-part-2-nodejs-modules-16666?l=i0DY0JjtC_3906218965
 
-const configurableMod = require('./configurable-module');
+//type node-inspector to launch it
+//destructuring & aliasing (git)
 
-const configuredA = configurableMod ({logPrefix: 'A> '});
-configuredA.log('test');
+// const {doSomething, getItDone: git} = require('./first-module');
+// doSomething();
+// git();
+
+const firstMod1 = require('./first-module');
+
+const firstMod2 = require('./first-module');
+
+console.log(firstMod1 === firstMod2);
+
+firstMod1.put('test data');
+console.log(firstMod2.get());
+
+// const configurableMod = require('./configurable-module');
+
+// const configuredA = configurableMod ({logPrefix: 'A> '});
+// configuredA.log('test');
+
+// const configuredB = configurableMod ({logPrefix: 'B> '});
+// configuredB.log('test 2');
